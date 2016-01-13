c1755f809b3e2ae341d6c202b1629cfb53ff2936
panser <gostroy@gmail.com>
2016-01-13 21:14:37 +0200

    add first portlet (Liferay MVC Application)

---
+++ My Hello World Portlet-portlet/docroot/WEB-INF/src/ua/org/gostroy/portlets/MyHelloWorld.java
@@ -0,0 +1,11 @@
+package ua.org.gostroy.portlets;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class MyHelloWorld
+ */
+public class MyHelloWorld extends MVCPortlet {
+ 
+
+}
