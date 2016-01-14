72bdfe5cdd129286f285f21abf0b9ec5e50fb748
panser <gostroy@gmail.com>
2016-01-14 20:41:37 +0200

    mvc_lesson6_step1: add admin portlet ( https://dev.liferay.com/develop/learning-paths/mvc/-/knowledge_base/6-2/creating-an-admin-portlet )

---
+++ adminGuestBook-portlet/docroot/WEB-INF/src/ua/org/gostroy/guestbook/portlet/GuestbookAdminPortlet.java
@@ -0,0 +1,11 @@
+package ua.org.gostroy.guestbook.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class GuestbookAdminPortlet
+ */
+public class GuestbookAdminPortlet extends MVCPortlet {
+ 
+
+}
