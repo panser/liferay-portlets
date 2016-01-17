587d2e73dd8a40aee65adc8fd0f85f3c7ecfa558
panser <gostroy@gmail.com>
2016-01-17 18:51:39 +0200

    mvc_lesson11_step1:  IMPLEMENTING WORKFLOW HANDLERS ( https://dev.liferay.com/develop/learning-paths/mvc/-/knowledge_base/6-2/implementing-workflow-handlers )

---
+++ adminGuestBook-portlet/docroot/WEB-INF/src/ua/org/gostroy/guestbook/workflow/GuestbokWorkflowHandler.java
@@ -0,0 +1,45 @@
+package ua.org.gostroy.guestbook.workflow;
+
+import java.io.Serializable;
+import java.util.Locale;
+import java.util.Map;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.BaseWorkflowHandler;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.security.permission.ResourceActionsUtil;
+import com.liferay.portal.service.ServiceContext;
+
+import ua.org.gostroy.guestbook.model.Guestbook;
+import ua.org.gostroy.guestbook.service.GuestbookLocalServiceUtil;
+
+public class GuestbookWorkflowHandler extends BaseWorkflowHandler {
+
+	@Override
+	public String getClassName() {
+
+		return CLASS_NAME;
+	}
+
+	@Override
+	public String getType(Locale locale) {
+		return ResourceActionsUtil.getModelResource(locale, getClassName());
+	}
+
+	@Override
+	public Object updateStatus(int status, Map<String, Serializable> workflowContext) throws PortalException, SystemException {
+
+		long userId = GetterUtil.getLong((String) workflowContext.get(WorkflowConstants.CONTEXT_USER_ID));
+		long guestbookId = GetterUtil.getLong((String) workflowContext.get(WorkflowConstants.CONTEXT_ENTRY_CLASS_PK));
+
+		ServiceContext serviceContext = (ServiceContext) workflowContext.get("serviceContext");
+
+		return GuestbookLocalServiceUtil.updateStatus(userId, guestbookId, status, serviceContext);
+
+	}
+
+	public static final String CLASS_NAME = Guestbook.class.getName();
+
+}
